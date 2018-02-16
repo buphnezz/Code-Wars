4c00b75a54770cb3a312d4f170ff30d96d116977
zachary schumpert <buphnezz@gmail.com>
2018-02-15 18:10:14 -0800

    solved code wars 04 simple validation of a username with regex

--- solution.js
+++ solution.js
@@ -1,13 +1,4 @@
-function insertDash(num) {
-  var numString = num.toString();
-  var returnString = numString[0];
-  for (var i = 1; i < numString.length; i++) {
-    if (parseInt(numString[i]) % 2 !== 0 && parseInt(numString[i - 1]) % 2 !== 0) {
-      returnString = returnString + '-' + numString[i];
-    }
-    else {
-      returnString = returnString + numString[i];
-    }
-  }
-  return returnString;
+function validateUsr(username) {
+  res = /^[a-z0-9_]{4,16}$/.test(username)
+  return res
 }
