f74da5df620d701fe7ef1ed0cda7c7128dcb508a
zachary schumpert <buphnezz@gmail.com>
2018-02-22 13:54:08 -0800

    INCOMPLETE code wars challenged

--- solution.js
+++ solution.js
@@ -1,5 +1,3 @@
-function disemvowel(str) {
-  let arr = str.split('');
-  const Vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u','U'];
-  return(arr.filter(letter => !Vowels.includes(letter)).join(''));
+function formatWords(words) {
+
 }
